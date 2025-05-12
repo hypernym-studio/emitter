1e8aaa5b1395b758c7f9311ea79de6e9da5fcfdf
Ivo Dolenc <ivodolenc@gmail.com>
2025-05-12 14:37:45 +0200

    chore: update main bundler config

--- bundler.config.ts
+++ bundler.config.ts
@@ -3,6 +3,21 @@ import { defineConfig } from '@hypernym/bundler'
 export default defineConfig({
   entries: [
     { input: './src/index.ts' },
+    { input: './src/index.ts', output: './dist/index.min.mjs', minify: true },
     { dts: './src/types.ts', output: './dist/index.d.mts' },
+    {
+      input: './src/index.ts',
+      output: './dist/index.iife.js',
+      name: 'Emitter',
+      format: 'iife',
+      minify: true,
+    },
+    {
+      input: './src/index.ts',
+      output: './dist/index.umd.js',
+      name: 'Emitter',
+      format: 'umd',
+      minify: true,
+    },
   ],
 })
