d3f302602f23493b5de1f6f9505b7b1ed5e580fd
Ivo Dolenc <ivodolenc@gmail.com>
2025-10-13 16:31:00 +0200

    chore: update output exts

--- bundler.config.ts
+++ bundler.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from '@hypernym/bundler'
 export default defineConfig({
   entries: [
     { input: './src/index.ts' },
-    { input: './src/index.ts', output: './dist/index.min.mjs', minify: true },
-    { dts: './src/types.ts', output: './dist/index.d.mts' },
+    { input: './src/index.ts', output: './dist/index.min.js', minify: true },
+    { dts: './src/types.ts', output: './dist/index.d.ts' },
     {
       input: './src/index.ts',
       output: './dist/index.iife.js',
